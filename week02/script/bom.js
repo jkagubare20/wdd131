b964a1c5cba72b7fb614089a466281f84a9867dd
jkagubare20 <kagubarejesus@gmail.com>
2025-05-12 18:52:22 +0200

    Add BOM example HTML, CSS, and JavaScript files

---
+++ week02/script/bom.js
@@ -0,0 +1,17 @@
+const input = document.querySelector('#favchap')
+
+const button = document.querySelector('button')
+
+const list = document.querySelector('ul')
+
+const listItem = document.createElement('li')
+
+const deleteButton = document.createElement('button')
+
+listItem.textContent = input.value
+
+deleteButton.textContent = '❌'
+
+listItem.append(deleteButton);
+
+list.append(li);
