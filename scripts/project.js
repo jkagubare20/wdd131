ae21cc5a33ea15912149e8c973628768727a4f83
jkagubare20 <kagubarejesus@gmail.com>
2025-06-10 16:44:03 +0200

    Refactor code structure for improved readability and maintainability

--- scripts/project.js
+++ scripts/project.js
@@ -13,63 +13,106 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
-const temples = [
+const posts = [
   {
     postTitle: "Post Title",
     date: "2005, August, 7",
     postDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    imageSrc: "images/placeholder.webp",
-    imageAlt: "Placeholder Image", 
+    imageSrc: "images/blogimg.webp",
+    imageAlt: "Image", 
   },
   {
     postTitle: "Post Title",
     date: "1888, May, 21",
     postDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    imageSrc: "images/placeholder.webp",
+    imageSrc: "images/blogimg2.jpeg",
+    imageAlt: "Image",
   },
   {
     postTitle: "Post Title",
-    dedicated: "2015, June, 7",
+    date: "2015, June, 7",
     postDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    imageSrc: "images/placeholder.webp",
+    imageSrc: "images/blogimg3.jpeg",
+    imageAlt: "Image",
   },
   {
     postTitle: "Post Title",
-    dedicated: "2020, May, 2",
+    date: "2020, May, 2",
     postDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    imageSrc: "images/placeholder.webp",
+    imageSrc: "images/blogimg4.webp",
+    imageAlt: "Image",
   },
   {
     postTitle: "Post Title",
-    dedicated: "1974, November, 19",
+    date: "1974, November, 19",
     postDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    imageSrc: "images/placeholder.webp",
+    imageSrc: "images/blogimg.webp",
+    imageAlt: "Image",
   },
   {
     postTitle: "Post Title",
-    dedicated: "1986, January, 10",
+    date: "1986, January, 10",
     postDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    imageSrc: "images/placeholder.webp",
+    imageSrc: "images/blogimg2.jpeg",
+    imageAlt: "Image",
     
   },
   {
     postTitle: "Post Title",
-    dedicated: "1983, December, 2", 
+    date: "1983, December, 2", 
     postDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    imageSrc: "images/placeholder.webp",
+    imageSrc: "images/blogimg3.jpeg",
+    imageAlt: "Image",
   },
     {
       postTitle: "Post Title",
-      dedicated: "1893, April, 6",
+      date: "1893, April, 6",
       postDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      imageSrc: "images/placeholder.webp",
+      imageSrc: "images/blogimg4.webp",  
+      imageAlt: "Image",
     },
     {
       postTitle: "Post Title",
-      dedicated: "2016, March, 20",
+      date: "2016, March, 20",
       postDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      imageSrc: "images/placeholder.webp",
+      imageSrc: "images/blogimg.webp",
+      imageAlt: "Image",
     }
-
-  
 ];
+
+function createPostCards(filteredPosts) {
+    const postCardsContainer = document.querySelector('.post-container');
+    
+    postCardsContainer.innerHTML = ''; // Clear previous cards
+
+    filteredPosts.forEach(post => {
+        const card = document.createElement('div');
+        card.classList.add('post-card');
+
+        const image = document.createElement('img');
+        image.src = post.imageSrc; // this is the image URL
+        image.alt = `${post.imageAlt} `; // alt text for the image
+        image.loading = 'lazy'; // lazy loading for performance
+        image.width = '300';
+        image.height = '200';
+
+        const date = document.createElement('p');
+        date.textContent = `${post.date}`;
+
+        const name = document.createElement('h3');
+        name.textContent = post.postTitle;
+
+        const description = document.createElement('p');
+        description.textContent = `${post.postDescription}`;
+
+        card.appendChild(image);
+        card.appendChild(date);
+        card.appendChild(name);
+        card.appendChild(description);
+
+        postCardsContainer.appendChild(card);
+    });
+
+}
+
+createPostCards(posts);
